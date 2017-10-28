d77fd75bd538a784634be698c6b5b28f159fc3c2
bolyke <olza99998@gmail.com>
2017-10-28 11:48:55 +0300

    booking modal

--- js/common.js
+++ js/common.js
@@ -1,30 +1,34 @@
 $(function () {
 	var $page = $('html, body');
-	$('a[href*="#"]').click(function () {
+	$('a[href*="#"]' && !"a[href='#modal-franchise']").click(function () {
 		$page.animate({
 			scrollTop: $($.attr(this, 'href')).offset().top - 67
 		}, 800);
 		return false;
 	});
+
 });
+
+
+
+
 
 $(".uk-inline").click(function () {
 	$(".cbp-af-header").hide();
-	// document.getElementsByClassName('cbp-af-header').style.display = 'none';
 });
 $("button[class='uk-lightbox-toolbar-icon']").click(function () {
 	$(".cbp-af-header").show();
-	// document.getElementsByClassName('cbp-af-header').style.display = 'block';
 });
-$("a[href='#modal-overflow']").click(function () {
+$("a[href='#modal-franchise']").click(function () {
 	$(".cbp-af-header").hide();
-	// document.getElementsByClassName('cbp-af-header').style.display = 'none';
 });
+$(".box-content-button").click(function () {
+	$(".cbp-af-header").hide();
+});
+
 $(".uk-modal-close").click(function () {
 	$(".cbp-af-header").show();
-	// document.getElementsByClassName('cbp-af-header').style.display = 'block';
 });
 $(".uk-close").click(function () {
 	$(".cbp-af-header").show();
-	// document.getElementsByClassName('cbp-af-header').style.display = 'block';
 });
