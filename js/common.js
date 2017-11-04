0bd3d273c2a666dc2f990e1f07c9651e84a92736
bolyke <olza99998@gmail.com>
2017-11-05 00:30:42 +0200

    flexible for mobile

--- js/common.js
+++ js/common.js
@@ -1,49 +1,62 @@
-$(function () {
-	var $page = $('html, body');
-	$('a[href*="#"]').click(function () {
-		$page.animate({
-			scrollTop: $( $.attr(this, 'href') ).offset().top - 95
-		}, 800);
-		return false;
+// $(function () {
+// 	var $page = $('html, body');
+// 	$('a[href*="#"]').click(function () {
+// 		$page.animate({
+// 			scrollTop: $( $.attr(this, 'href') ).offset().top - 95
+// 		}, 800);
+// 		return false;
+// 	});
+// });
+
+$(document).ready(function() {
+	$(".menu-link").on("click", function() {
+			$("html, body").stop().animate({
+				scrollTop: $( $.attr(this, 'href') ).offset().top - 95
+			}, 800),
+			$(".nav").removeClass("act"),
+			event.preventDefault()
+	}),
+	$(".menuBtn").on("click", function() {
+			$(".nav").toggleClass("act")
 	});
 });
 
 
+// $(function () {
+// 	$(document).ready(function () {
+// 		// menu click event
+// 		$('.menuBtn').click(function () {
+// 			$(this).toggleClass('act');
+// 			if ($(this).hasClass('act')) {
+// 				$('.nav').addClass('act');
 
-$(function () {
-	$(document).ready(function () {
-		// menu click event
-		$('.menuBtn').click(function () {
-				$(this).toggleClass('act');
-				if ($(this).hasClass('act')) {
-						$('.nav').addClass('act');
+// 			}
+// 			else {
+// 				$('.nav').removeClass('act');
+// 			}
 
-				}
-				else {
-					$('.nav').removeClass('act');
-				}
+// 		});
 
-		});
+// 	});
+// });
 
-	});	
-});
 
 
 
 ( function( window ) {
-	
+
 	'use strict';
-	
+
 	// class helper functions from bonzo https://github.com/ded/bonzo
-	
+
 	function classReg( className ) {
 	  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
 	}
-	
+
 	// classList support for class management
 	// altho to be fair, the api sucks because it won't accept multiple classes at once
 	var hasClass, addClass, removeClass;
-	
+
 	if ( 'classList' in document.documentElement ) {
 	  hasClass = function( elem, c ) {
 		return elem.classList.contains( c );
@@ -68,12 +81,12 @@ $(function () {
 		elem.className = elem.className.replace( classReg( c ), ' ' );
 	  };
 	}
-	
+
 	function toggleClass( elem, c ) {
 	  var fn = hasClass( elem, c ) ? removeClass : addClass;
 	  fn( elem, c );
 	}
-	
+
 
 	var classie = {
 	  // full names
@@ -87,7 +100,7 @@ $(function () {
 	  remove: removeClass,
 	  toggle: toggleClass
 	};
-	
+
 	// transport
 	if ( typeof define === 'function' && define.amd ) {
 	  // AMD
@@ -96,6 +109,5 @@ $(function () {
 	  // browser global
 	  window.classie = classie;
 	}
-	
+
 	})( window );
-	
